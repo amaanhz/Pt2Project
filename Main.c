e255438b2ecbaac834271d7cc3ab6c7d064dd051
amaanhz <amaanhamza15@gmail.com>
2024-12-23 20:25:15 +0000

    Trying to find where to save time

--- Main.c
+++ Main.c
@@ -25,7 +25,7 @@ int main(int argc, char* argv[]) {
 	struct timespec start, end;
 
 	clock_gettime(CLOCK_MONOTONIC, &start);
-	//DijkstraAPSP(graph);
+	DijkstraAPSP(graph);
 	clock_gettime(CLOCK_MONOTONIC, &end);
 
 	double time_spent = (end.tv_sec - start.tv_sec);
@@ -44,16 +44,16 @@ int main(int argc, char* argv[]) {
 
 	Result** b_results = NULL;
 
-	/*clock_gettime(CLOCK_MONOTONIC, &start);
+	clock_gettime(CLOCK_MONOTONIC, &start);
 	b_results = BMFordAPSP(graph);
 	clock_gettime(CLOCK_MONOTONIC, &end);
 
 	time_spent = (end.tv_sec - start.tv_sec);
 	time_spent += (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-	printf("Runtime for Bellman-Ford_APSP (Seq): %f\n", time_spent);*/
+	printf("Runtime for Bellman-Ford_APSP (Seq): %f\n", time_spent);
 
 	clock_gettime(CLOCK_MONOTONIC, &start);
-	//b_results = BMFordAPSP_mt_a(graph, 16);
+	b_results = BMFordAPSP_mt_a(graph, 16);
 	clock_gettime(CLOCK_MONOTONIC, &end);
 
 	time_spent = (end.tv_sec - start.tv_sec);
@@ -100,15 +100,15 @@ int main(int argc, char* argv[]) {
 
 	printf("\n");
 
-	//printf("Results for Dijkstra and BMFord are %s\n", resultsEq(d_results, b_results, graph->size) ? "equal" : "non-equal");
+	printf("Results for Dijkstra and BMFord are %s\n", resultsEq(d_results, b_results, graph->size) ? "equal" : "non-equal");
 	printf("Results for Dijkstra and FW are %s\n", resultsEq(d_results, f_results, graph->size) ? "equal" : "non-equal");
-	//printf("Results for BMFord and FW are %s\n", resultsEq(b_results, f_results, graph->size) ? "equal" : "non-equal");
+	printf("Results for BMFord and FW are %s\n", resultsEq(b_results, f_results, graph->size) ? "equal" : "non-equal");
 
 	///////////////
 
 	// Freeing Memory so compiler stops shouting //
 
-	//freeResults(b_results, graph->size);
+	freeResults(b_results, graph->size);
 	freeResults(d_results, graph->size);
 	freeResults(f_results, graph->size);
 	freeGraph(graph);
