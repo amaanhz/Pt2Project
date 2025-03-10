e7ead0aeccb15f849f8223f51f1dff70cfdf0092
amaanhz <amaanhamza15@gmail.com>
2025-03-10 16:33:25 +0000

    FWarsh GPU works but only if I set block_length to 1??? (wtf)

--- CUDA/FWarsh.cu
+++ CUDA/FWarsh.cu
@@ -27,7 +27,7 @@ Vec2::Vec2(int x, int y) : x(x), y(y) {}
 Triple::Triple(Vec2 p1, Vec2 p2, Vec2 p3) : p1(p1), p2(p2), p3(p3) { }
 
 __device__ void block_loop (int* d1, int* p1, int* d2, int* d3, int* p3, int rowIndex,
-    int maxIndex, int bl, int cell) {
+    int maxIndex, int cell) {
     // all pointers are pointers to shared block memory
     // only contains block values, not whole matrix
     for (int k = 0; k < maxIndex; k++) {
@@ -39,9 +39,9 @@ __device__ void block_loop (int* d1, int* p1, int* d2, int* d3, int* p3, int row
         if (t1 != INT_MAX && t2 != INT_MAX) {
             int t = t1 + t2;
             if (t < d1[cell]) {
-                printf("t = %d, t < d1[%d] = %d\n", t, cell, d1[cell]);
+                //printf("t = %d, t < d1[%d] = %d\n", t, cell, d1[cell]);
                 d1[cell] = t;
-                printf("Setting p1[%d] = p3[%d] = %d\n\n", cell, kRow + threadIdx.y, p3[kRow + threadIdx.y]);
+                //printf("Setting p1[%d] = p3[%d] = %d\n\n", cell, kRow + threadIdx.y, p3[kRow + threadIdx.y]);
                 p1[cell] = p3[kRow + threadIdx.y];
             }
         }
@@ -88,7 +88,6 @@ __global__ void dep_block (int b, int num_blocks, int bl, int graphLength, int r
     //printf("hello\n");
     int blockSize = bl * bl;
 
-    int blocksDown = blockSize * (num_blocks - 1) + maxIndex * maxIndex;
     int rowsDown = graphLength * num_blocks;
 
 
@@ -99,10 +98,10 @@ __global__ void dep_block (int b, int num_blocks, int bl, int graphLength, int r
     extern __shared__ int dist[];
     int* prev = dist + blockSize;
 
-    //int intoDevBlock = b * blocksDown + threadIdx.x * (rowsDown - 1) + b * bl + threadIdx.y;
-    int intoDevBlock = cell;
+    int intoDevBlock = graphLength * bl * b + graphLength * threadIdx.x + bl * b + threadIdx.y;
+    //int intoDevBlock = cell
 
-    printf("threadIdx.x = %d, threadIdx.y = %d, cell = %d, intoDevBlock = %d\n", threadIdx.x, threadIdx.y, cell, intoDevBlock);
+    //printf("threadIdx.x = %d, threadIdx.y = %d, cell = %d, intoDevBlock = %d\n", threadIdx.x, threadIdx.y, cell, intoDevBlock);
 
     //printf("intoDevBlock for %d, %d = %d, dev_dist[%d] = %d, dev_prev[%d] = %d\n",
     //    threadIdx.x, threadIdx.y, intoDevBlock, intoDevBlock, dev_dist[intoDevBlock], intoDevBlock, dev_prev[intoDevBlock]);
@@ -113,24 +112,24 @@ __global__ void dep_block (int b, int num_blocks, int bl, int graphLength, int r
 
     __syncthreads(); // make sure shared memory is fully initialised
 
-    if (threadIdx.x == 0 && threadIdx.y == 0) {
+    /*if (threadIdx.x == 0 && threadIdx.y == 0) {
         printf("Before loop:\n");
-        printArr(dist, prev, rem * rem);
+        printArr(dist, prev, maxIndex * maxIndex);
         printf("\n");
-    }
+    }*/
 
-//if (threadIdx.x == 0 && threadIdx.y == 0) {
 
-        block_loop(dist, prev, dist, dist, prev, rowIndex, maxIndex, bl, cell);
-    //}
+    block_loop(dist, prev, dist, dist, prev, rowIndex, maxIndex, cell);
+
     //printf("threadIdx.x = %d, threadIdx.y = %d, intoDevBlock = %d bl = %d\n", threadIdx.x, threadIdx.y
     //, intoDevBlock, bl);
     __syncthreads();
-    if (threadIdx.x == 0 && threadIdx.y == 0) {
+
+    /*if (threadIdx.x == 0 && threadIdx.y == 0) {
         printf("After loop:\n");
-        printArr(dist, prev, rem * rem);
+        printArr(dist, prev, maxIndex * maxIndex);
         printf("\n");
-    }
+    }*/
     // write-back
     dev_dist[intoDevBlock] = dist[cell];
     dev_prev[intoDevBlock] = prev[cell];
@@ -138,26 +137,27 @@ __global__ void dep_block (int b, int num_blocks, int bl, int graphLength, int r
 
 
 
-__global__ void pdep_blocks (int b, int num_blocks, int bl, int rem, int* dev_dist, int* dev_prev) {
+__global__ void pdep_blocks (int b, int num_blocks, int bl, int graphLength, int rem, int* dev_dist, int* dev_prev) {
     // B[b, i], B[b, b], B[b, i]
     // B[i, b], B[i, b], B[b, b]
 
-    printf("hello2\n");
+    //if (threadIdx.x == 0 && threadIdx.y == 0) printf("blockIdx.x = %d, blockIdx.y = %d\n", blockIdx.x, blockIdx.y);
     if ( threadIdx.x >= bl || threadIdx.y >= bl ) return;
 
     int blockSize = bl * bl;
 
     // find out which block we are
     int blockX; int blockY;
-    if ( blockIdx.x >= b ) {
-        blockX = blockIdx.x + 1;
-        if (blockIdx.y == b) { blockY = b; }
-        else { return; }
+    if ( blockIdx.y == b ) {
+        blockX = blockIdx.x < b ? blockIdx.x : blockIdx.x + 1;
+        blockY = b;
     }
-    else if ( blockIdx.y >= b ) {
-        blockY = blockIdx.y + 1;
-        if (blockIdx.x == b) { blockX = b; }
-        else { return; }
+    else if ( blockIdx.x == b ) {
+        blockY = blockIdx.y < b ? blockIdx.y : blockIdx.y + 1;
+        blockX = b;
+    }
+    else {
+        return;
     }
 
     // need to fetch our block, and block B[b, b]
@@ -171,7 +171,8 @@ __global__ void pdep_blocks (int b, int num_blocks, int bl, int rem, int* dev_di
 
     int rowIndex = threadIdx.x * bl;
 
-    int devIndexIn = b * blocksDown + threadIdx.x * rowsDown + b * bl + threadIdx.y;
+    int devIndexIn = graphLength * bl * b + graphLength * threadIdx.x + bl * b + threadIdx.y;
+    //printf("b = %d, blockX = %d, blockY = %d, devIndexIn = %d\n", b, blockX, blockY, devIndexIn);
     int cell = rowIndex + threadIdx.y;
 
     dist[cell] = dev_dist[devIndexIn];
@@ -181,8 +182,10 @@ __global__ void pdep_blocks (int b, int num_blocks, int bl, int rem, int* dev_di
     int* dist_1; int* dist_2; int* dist_3;
     int* prev_1;              int* prev_3;
 
+    // maybe we should have seperate kernels for the two types of pdep blocks? Avoids branching
+
     if (blockX == b) { // B[b, i], B[b, b], B[b, i]
-        devIndexIn = b * blocksDown + threadIdx.x * rowsDown + blockY * bl + threadIdx.y;
+        devIndexIn = graphLength * bl * b + graphLength * threadIdx.x + bl * blockY + threadIdx.y;
         dist_i[cell] = dev_dist[devIndexIn];
         prev_i[cell] = dev_prev[devIndexIn];
 
@@ -190,7 +193,7 @@ __global__ void pdep_blocks (int b, int num_blocks, int bl, int rem, int* dev_di
         prev_1 = prev_i;                prev_3 = prev_i;
     }
     else if ( blockY == b ) { // B[i, b], B[i, b], B[b, b]
-        devIndexIn = blockX * bl + rowIndex + b + threadIdx.y;
+        devIndexIn = graphLength * bl * blockX + graphLength * threadIdx.x + bl * b + threadIdx.y;
         dist_i[cell] = dev_dist[devIndexIn];
         prev_i[cell] = dev_prev[devIndexIn];
 
@@ -207,15 +210,15 @@ __global__ void pdep_blocks (int b, int num_blocks, int bl, int rem, int* dev_di
 
     __syncthreads(); // ensure all initialised
 
-    block_loop(dist_1, prev_1, dist_2, dist_3, prev_3, rowIndex, maxIndex, bl, cell);
+    block_loop(dist_1, prev_1, dist_2, dist_3, prev_3, rowIndex, maxIndex, cell);
 
     dev_dist[devIndexIn] = dist_i[cell];
     dev_prev[devIndexIn] = prev_i[cell];
 }
 
-__global__ void indep_blocks (int b, int num_blocks, int bl, int rem, int* dev_dist, int* dev_prev) {
+__global__ void indep_blocks (int b, int num_blocks, int bl, int graphLength, int rem, int* dev_dist, int* dev_prev) {
     // B[i, j], B[i, b], B[b, j]
-    printf("hello3\n");
+    //if ( threadIdx.x == 0 && threadIdx.y == 0) printf("blockIdx.x = %d, blockIdx.y = %d\n", blockIdx.x, blockIdx.y);
     if ( threadIdx.x >= bl || threadIdx.y >= bl ) return;
 
     int blockSize = bl * bl;
@@ -224,8 +227,6 @@ __global__ void indep_blocks (int b, int num_blocks, int bl, int rem, int* dev_d
     int blockX = blockIdx.x + (blockIdx.x >= b);
     int blockY = blockIdx.y + (blockIdx.y >= b);
 
-    int blocksDown = blockSize * num_blocks;
-    int rowsDown = bl * num_blocks;
     int rowIndex = threadIdx.x * bl;
     int cell = rowIndex + threadIdx.y;
 
@@ -243,15 +244,16 @@ __global__ void indep_blocks (int b, int num_blocks, int bl, int rem, int* dev_d
 
     // populate shared memory
     int sharedIndexIn = rowIndex + threadIdx.y;
-    int devIndexIn = blockX * blocksDown + threadIdx.x * rowsDown + blockY * bl + threadIdx.y;
+    int devIndexIn = graphLength * bl * blockX + graphLength * threadIdx.x + bl * blockY + threadIdx.y;
+    //printf("blockX = %d, blockY = %d, devIndexIn = %d\n", blockX, blockY, devIndexIn);
     dist_1[sharedIndexIn] = dev_dist[devIndexIn]; prev_1[sharedIndexIn] = dev_prev[devIndexIn];
-    dist_2[sharedIndexIn] = dev_dist[blockX * blocksDown + threadIdx.x * rowsDown + b * bl + threadIdx.y];
-    int devIndexIn_3 = b * blocksDown + threadIdx.x * rowsDown + blockY * bl + threadIdx.y;
+    dist_2[sharedIndexIn] = dev_dist[graphLength * bl * blockX + graphLength * threadIdx.x + bl * b + threadIdx.y];
+    int devIndexIn_3 = graphLength * bl * b + graphLength * threadIdx.x + bl * blockY + threadIdx.y;
     dist_3[sharedIndexIn] = dev_dist[devIndexIn_3]; prev_3[sharedIndexIn] = dev_prev[devIndexIn_3];
 
     __syncthreads();
 
-    block_loop(dist_1, prev_1, dist_2, dist_3, prev_3, rowIndex, maxIndex, bl, cell);
+    block_loop(dist_1, prev_1, dist_2, dist_3, prev_3, rowIndex, maxIndex, cell);
 
     dev_dist[devIndexIn] = dist_1[sharedIndexIn];
     dev_prev[devIndexIn] = prev_1[sharedIndexIn];
@@ -272,7 +274,7 @@ Result** cuda_FWarsh(GraphMatrix& graph, int block_length) {
             if (graph[rowIndex + c] != INT_MAX) { prev[rowIndex + c] = r; } // set previous as in graph
         }
     }
-    printArr(graph.GetMatrix(), prev.GetMatrix(), matSize);
+    //printArr(graph.GetMatrix(), prev.GetMatrix(), matSize);
     gpuErrchk(cudaMemcpy(dev_prev, &prev[0], sizeof(int) * matSize, cudaMemcpyHostToDevice));
 
     int num_blocks = (graph.GetSize() + block_length - 1) / block_length; // ceiling the value (1 axis)
@@ -285,14 +287,15 @@ Result** cuda_FWarsh(GraphMatrix& graph, int block_length) {
     dim3 pdep_dim(num_blocks - 1, num_blocks - 1);
 
     int indep_count = (num_blocks * num_blocks) - pdep_count - 1;
-    dim3 indep_dim(indep_count / 2, indep_count / 2);
+    dim3 indep_dim(num_blocks - 1, num_blocks - 1);
 
     size_t memsize = sizeof(int) * block_length * block_length * 2;
 
+    size_t free, totalmem;
     for (int block = 0; block < num_blocks; block++) {
         dep_block<<<1, block_threads, memsize>>>(block, num_blocks, block_length, graphSize, rem, dev_dist, dev_prev);
-        pdep_blocks<<<pdep_dim, block_threads, memsize * 2>>>(block, num_blocks, block_length, rem, dev_dist, dev_prev);
-        indep_blocks<<<indep_dim, block_threads, memsize * 2 + memsize / 2>>>(block, num_blocks, block_length, rem, dev_dist, dev_prev);
+        pdep_blocks<<<pdep_dim, block_threads, memsize * 2>>>(block, num_blocks, block_length, graphSize, rem, dev_dist, dev_prev);
+        indep_blocks<<<indep_dim, block_threads, memsize * 2 + memsize / 2>>>(block, num_blocks, block_length, graphSize, rem, dev_dist, dev_prev);
     }
 
     cudaDeviceSynchronize();
--- Main.cpp
+++ Main.cpp
@@ -12,18 +12,24 @@
 
 int main(int argc, char* argv[]) {
 
-    const char* graph_path = "graphs/testgraph";
+    const char* graph_path = "graphs/USairport500";
 
     struct timespec start, end;
     //GraphSearch("graphs/USairport500");
     auto graph = GraphMatrix(graph_path);
     //graph.printGraph();
+    clock_gettime(CLOCK_MONOTONIC, &start);
     Result** ground_truth = FWarsh_mt(fileparse(graph_path), 10, 16);
-    printResults(ground_truth, graph.GetSize());
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    double time_spent = (end.tv_sec - start.tv_sec);
+    time_spent += (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+    printf("\nRuntime for FWarsh (CPU): %f\n", time_spent);
+    //printResults(ground_truth, graph.GetSize());
 
     clock_gettime(CLOCK_MONOTONIC, &start);
     //Result** results = cuda_DijkstraAPSP(graph);
-    Result** results = cuda_FWarsh(graph, 32);
+    Result** results = cuda_FWarsh(graph, 1);
     clock_gettime(CLOCK_MONOTONIC, &end);
 
     //int test[13] = {-2, 1, 3, 3, 3, -9, -3, -1, 10, 11, 12,  2, 0};
@@ -31,15 +37,16 @@ int main(int argc, char* argv[]) {
     //fastmin(test, mask, 13);
 
     //printResults(results, graph.GetSize());
-    double time_spent = (end.tv_sec - start.tv_sec);
+
+    time_spent = (end.tv_sec - start.tv_sec);
     time_spent += (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-    printf("\nRuntime for Dijkstra_APSP (GPU): %f\n", time_spent);
+    printf("\nRuntime for FWarsh (GPU): %f\n", time_spent);
     //printResult(results[498], 498, graph.GetSize());
 
     //printResult(ground_truth[7], 7, graph.GetSize());
     //printResult(results[7], 7, graph.GetSize());
 
-    printf("Results for GPU_Dijkstra and Floyd-Warshall are %s\n",
+    printf("Results for GPU_Fwarsh and CPU_FWarsh are %s\n",
            resultsEq(ground_truth, results, graph.GetSize()) ? "equal" : "non-equal");
 
     printf("Done\n");
